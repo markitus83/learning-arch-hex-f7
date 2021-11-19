a419057fed67d44f2d88acc2971e616a2bbbeb89
Marc Ginovart <mginovart@leadtech.com>
2021-11-19 15:06:39 +0100

    update index

--- code/index.php
+++ code/index.php
@@ -1,8 +1,11 @@
 <?php
 
+use Fut7\Domain\Response\Season\UpdateSeasonResponse;
 use Fut7\UserInterface\Controller\Season\CRUD\CreateSeasonController;
 use Fut7\UserInterface\Controller\Season\CRUD\DeleteSeasonController;
 use Fut7\UserInterface\Controller\Season\CRUD\FindSeasonController;
+use Fut7\UserInterface\Controller\Season\CRUD\SearchSeasonController;
+use Fut7\UserInterface\Controller\Season\CRUD\UpdateSeasonController;
 
 require_once ("vendor/autoload.php");
 
@@ -16,3 +19,9 @@ echo "Learning DDD from scratch".PHP_EOL.PHP_EOL;
 
 //$controller = new DeleteSeasonController();
 //$controller->execute();
+
+//$controller = new SearchSeasonController();
+//$controller->execute();
+
+$controller = new UpdateSeasonController();
+$controller->execute();
