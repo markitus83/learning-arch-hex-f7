06c9d297d2d3c4454038ea541b6579357a2b6d04
Marc Ginovart <mginovart@leadtech.com>
2021-11-26 20:57:57 +0100

    add Application/... code (Tournament)

---
+++ code/Fut7/Application/Tournament/CRUD/Create/CreateTournamentCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Fut7\Application\Tournament\CRUD\Create;
+
+use Fut7\Domain\Entity\Season\Season;
+
+class CreateTournamentCommand
+{
+    private string $id;
+    private string $name;
+    private Season $season;
+
+    public function __construct($id, $name, $season)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->season = $season;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function season(): Season
+    {
+        return $this->season;
+    }
+
+}
