c4f547775d8244bbeaf8e72aac333469ad8cc3d2
Marc Ginovart <mginovart@leadtech.com>
2021-12-03 14:34:21 +0100

    add new code

---
+++ code/Fut7/Application/Tournament/CRUD/Find/FindTournamentQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Fut7\Application\Tournament\CRUD\Find;
+
+class FindTournamentQuery
+{
+    private string $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+}
---
+++ code/Fut7/UserInterface/Controller/Tournament/CRUD/FindTournamentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Fut7\UserInterface\Controller\Tournament\CRUD;
+
+use Fut7\Application\Tournament\CRUD\Find\FindTournamentQuery;
+use Fut7\Application\Tournament\CRUD\Find\FindTournamentUseCase;
+use Fut7\Domain\Exception\Tournament\TournamentNotFoundException;
+use Fut7\Infrastructure\Persistence\Shared\CsvRepository;
+use Fut7\Infrastructure\Persistence\Tournament\CsvTournamentRepository;
+
+class FindTournamentController
+{
+    public function execute()
+    {
+        $randomIdToFind = ['123trf33', '61a9d3d6acac9'];
+        $id = $randomIdToFind[rand(0,1)];
+        echo 'Trying to find Tournament.id = '.$id.PHP_EOL;
+
+        $query = new FindTournamentQuery($id);
+
+        try {
+            $repository = new CsvTournamentRepository(new CsvRepository(CsvTournamentRepository::repositoryFile()));
+            $findTournament = new FindTournamentUseCase($repository);
+            $response = $findTournament->execute($query);
+
+            echo PHP_EOL.$response->getResponse()['message'];
+            echo PHP_EOL.$response->getResponse()['data'];
+        } catch (TournamentNotFoundException $tournamentNotFoundException) {
+            echo PHP_EOL.'Error found trying to find Tournament: '.$id.' NOT FOUND';
+        }
+    }
+}
