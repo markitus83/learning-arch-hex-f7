747106c8989d0ef0f9468cb8090b1400e4510bd7
Marc Ginovart <mginovart@leadtech.com>
2022-02-17 16:36:47 +0100

    doing tests

--- tests/Fut7/Unit/Domain/Season/SeasonTest.php
+++ tests/Fut7/Unit/Domain/Season/SeasonTest.php
@@ -4,38 +4,39 @@ namespace Fut7\Unit\Domain\Season;
 
 use Fut7\Domain\Entity\Season\Season;
 use http\Exception\InvalidArgumentException;
+use Fut7\Infrastructure\Shared\Utils\Uuid;
 use PHPUnit\Framework\TestCase;
 
 class SeasonTest extends TestCase
 {
     public function testCreateSeasonWithBasicValuesFromScratch()
     {
-        $id = uniqid();
+        $uuid = new Uuid();
         $name = 'seasonFake';
 
-        $season = Season::createFromScratch($id, $name);
+        $season = Season::createFromScratch($uuid, $name);
         $this->assertInstanceOf(Season::class, $season, 'Failed test create Season with basic values');
     }
 
     public function testCreateSeasonWithoutNameFromScratch()
     {
-        $id = uniqid();
+        $uuid = new Uuid();
         $name = null;
 
         $this->expectException(\TypeError::class);
 
-        $season = Season::createFromScratch($id, $name);
+        $season = Season::createFromScratch($uuid, $name);
         $this->assertInstanceOf(Season::class, $season, 'Failed test create Season without name');
     }
 
     public function testCreateSeasonWithoutIdFromScratch()
     {
-        $id = null;
+        $uuid = null;
         $name = 'seasonFake';
 
         $this->expectException(\TypeError::class);
 
-        $season = Season::createFromScratch($id, $name, 5656);
+        $season = Season::createFromScratch($uuid, $name, 5656);
         $this->assertInstanceOf(Season::class, $season, 'Failed test create Season without id');
     }
 }
